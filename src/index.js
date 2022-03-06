0da716ab0ef1286f7e62707097bb790d5f77b1b4
LucasMiguel <lucasoliveira129@hotmail.com>
2022-03-06 00:59:21 -0300

    Finalizado a parte de criar a contagem do tempo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MainWindow from './templates/mainWindow';
+import App from './templates/app';
 
-ReactDOM.render(
-  <div id='setDarkMode' className=''>
-    <div className='bg-[#F0F3F3] dark:bg-[#011627]  h-screen'>
-      <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainWindow />} />
-          </Routes>
-        </BrowserRouter>
-      </React.StrictMode>
-    </div>
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/templates/app.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainWindow from './mainWindow';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.concentrationTime = 60;
+        this.breakTime = 60;
+        this.longTime = 120;
+        this.totalCycles = 3;
+        this.state = {
+            time: this.concentrationTime,
+            cyclesCount: 1,
+            isPlay: true,
+            isConcentration: true
+        }
+    }
+    /** Função que irá formatar o tempo de segundos para minutos
+      * @param {*} duration O tempo em segundos
+      * @returns 
+      */
+    timeFormat = (duration) => {
+        // Hours, minutes and seconds
+        var hrs = ~~(duration / 3600);
+        var mins = ~~((duration % 3600) / 60);
+        var secs = ~~duration % 60;
+
+        // Output like "1:01" or "4:03:59" or "123:03:59"
+        var ret = "";
+
+        if (hrs > 0) {
+            ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+        }
+
+        ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+        ret += "" + secs;
+        return ret;
+    }
+
+    /** Função que irá fazer a contagem do tempo em forma decrescente */
+    countTime() {
+        if (this.state.time === 0) {
+            this.setState({
+                time: this.state.isConcentration ? (this.totalCycles === this.state.cyclesCount ? this.longTime : this.breakTime) : this.concentrationTime,
+                isConcentration: !this.state.isConcentration,
+                cyclesCount: this.totalCycles === this.state.cyclesCount ? 0 : this.state.isConcentration ? this.state.cyclesCount : this.state.cyclesCount + 1,
+                isPlay: !this.state.isPlay
+            });
+            clearInterval(this.isCount);
+            console.log(this.state.isPlay);
+        } else {
+            this.setState({
+                time: this.state.time - 1
+            });
+        }
+    }
+
+    /**Função que irá controlar a ação de clique do botão de play e pause */
+    playPauseClick = () => {
+        this.setState({
+            isPlay: !this.state.isPlay
+        });
+        if (this.state.isPlay) {
+            this.isCount = setInterval(() => this.countTime(), 100);
+        } else {
+            clearInterval(this.isCount);
+        }
+    }
+
+    /** Função que irá resetar o tempo*/
+    resetClick = () => {
+        this.setState({
+            time: this.timeConcentration
+        })
+    }
+
+    render() {
+        return (
+            <div id='setDarkMode' className=''>
+                <div className='bg-[#F0F3F3] dark:bg-[#011627]  h-screen'>
+                    <React.StrictMode>
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="/" element={
+                                    <MainWindow
+                                        time={this.timeFormat(this.state.time)}
+                                        isConcentration={this.state.isConcentration}
+                                        totalCycles={this.totalCycles}
+                                        cyclesCount={this.state.cyclesCount}
+                                        isPlay={this.state.isPlay}
+                                        playPauseClick={this.playPauseClick}
+                                        resetClick={this.resetClick}
+                                    />
+                                } />
+                            </Routes>
+                        </BrowserRouter>
+                    </React.StrictMode>
+                </div>
+            </div>
+        );
+    }
+}
